c27234bc7c83e1aef0f1d6a47184a7f9deca854c
stephen.ransom <stephen.ransom@ruralsourcing.com>
2018-04-19 11:24:35 -0600

    added settings file

--- SlackNotifyBot.py
+++ SlackNotifyBot.py
@@ -1,8 +1,31 @@
 # relies on python slackclient
 # install with pip install slackclient
+# Should ideally be run as a daemon or service
+# Reads configuration from a file, if file does not exist, runs on built in defaults
+
 
 import sys
 import os
 from slackclient import SlackClient
 
-print("hello")
+  
+
+class Monitor():
+    watchDirectory = "."
+
+    def loadSettings():
+        try:
+            #open file
+            print("opening")
+        except:
+            #file not found, load defaults
+            print("Could not open settings")
+
+    print("I'm watching")
+
+class Alerter:
+    print("This is an alert")
+
+
+if __name__ == '__main__':
+    monitor = Monitor()
