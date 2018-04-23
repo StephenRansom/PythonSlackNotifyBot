8fc08a6a6e57c71b5a180b05ba12c810efbc9c19
stephen.ransom <stephen.ransom@ruralsourcing.com>
2018-04-23 15:14:13 -0600

    cleanup, added post to slack on error

--- SlackNotifyBot.py
+++ SlackNotifyBot.py
@@ -7,6 +7,7 @@
 import sys
 import os
 import time
+import datetime
 import configparser
 from slackclient import SlackClient
 
@@ -25,7 +26,7 @@ class Monitor():
             'slackReplyBroadcast' : 'True',
             'defaultValue' : '42',
             'slackBotUsername' : "Alert Bot"            
-        }
+        }        
         try:
             #open file
             config.read("settings.cfg")
@@ -44,13 +45,16 @@ class Monitor():
             self.slackBotUsername = config.get("Slack Settings", "slackBotUsername")
             self.slackIconEmoji= config.get("Slack Settings", "slackIconEmoji")
             self.slackReplyBroadcast= config.getboolean("Slack Settings", "slackReplyBroadcast")          
-        except configparser.NoOptionError as e:
+        except (configparser.NoOptionError, configparser.NoSectionError) as e:
             self.exit("encountered Exception {}".format(e))
 
     def exit(self, message):
         self.log.write(message + '\nExiting {}'.format(time.ctime()))
         self.log.flush()
-        sys.exit(message + '\nExiting')
+        try:
+            self.send_alert(self.send_alert(self.slackChannelName, message + " <!channel>", self.slackBotUsername, self.slackIconEmoji, self.slackReplyBroadcast))
+        finally:
+            sys.exit(message + '\nExiting')
 
 
     def initialize_slack_client(self):
@@ -72,20 +76,18 @@ class Monitor():
             if this is the first check that returned a failure, send alert 
             if we have just sent an alert, wait until we exceed the errorTolerance to generate another alert to avoid spam
         """
-        print("Error count: {}".format(self.contiguousErrorCount))
+
         if(self.contiguousErrorCount == self.contiguousErrorsUntilAlert): 
             self.send_alert(self.slackChannelName, self.slackMessageText, self.slackBotUsername, self.slackIconEmoji, self.slackReplyBroadcast)
-            print("Alert!")
         elif(self.contiguousErrorCount >= self.contiguousErrorsUntilAlert + self.contiguousErrorsAfterAlert):
             self.contiguousErrorCount = -1
-            print("resetting error count")
      
         self.contiguousErrorCount += 1
 
     
     def send_alert(self, slackChannelName, slackMessageText, slackBotUsername, slackIconEmoji, slackReplyBroadcast):
         """Sends alert to Slack Channel"""
-        self.log.write("ALERT! There are no new files!\n")
+        self.log.write(slackMessageText + " {}\n".format(time.ctime()))
         self.log.flush()
         
 
@@ -102,7 +104,6 @@ class Monitor():
             attempts to raise alert via a call to processError if file count is not increasing
         """
         newFileCount = sum(1 for f in os.listdir(self.watchDirectory) if os.path.isfile(os.path.join(self.watchDirectory, f)) and f[0] != '.')
-        self.log.write("current file count: {} \n".format(newFileCount))
 
         if(self.fileCount < newFileCount):
             self.fileCount = newFileCount
@@ -116,12 +117,14 @@ class Monitor():
             while True:
                 time.sleep(self.checkInterval)
                 self.update_file_count()
+        except (KeyboardInterrupt, SystemExit) as e:
+            self.exit("Encountered exception {}".format(e))
         except BaseException as e:
             self.exit("Encountered exception {}".format(e))
             
     
-    def __init__(self):  
-        self.log = open("./SlackNotifyLog.txt","w")
+    def __init__(self, configFile):  
+        self.log = open(configFile,"w")
         self.log.write("------Initializing------\n")
         self.load_settings()
         self.initialize_slack_client()
@@ -130,5 +133,5 @@ class Monitor():
       
 
 if __name__ == '__main__':
-    monitor = Monitor()
+    monitor = Monitor("./SlackNotifyLog.txt")
     monitor.run()
